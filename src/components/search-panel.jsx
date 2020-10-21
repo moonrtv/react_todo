04b47f8771b748b93d91b9666aff8bb7470f1df1
Тарас Родионов <trodionov@megalabs.ru>
2020-10-21 18:36:27 +0300

    add bootstrap

--- src/components/search-panel.jsx
+++ src/components/search-panel.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import './search-panel.css';
+
 const SearchPanel = () => {
-	return <input placeholder="Type to search info" />;
+	return (
+		<input
+			type="text"
+			className="form-control search-input"
+			placeholder="type to search"
+		/>
+	);
 };
 
 export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import ReactDOM from 'react-dom';
 import AppHeader from './components/app-header';
 import SearchPanel from './components/search-panel';
 import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
 
+import './index.css';
 import todoData from './data.json';
 
 const App = () => {
-	return (<div>
-		<AppHeader />
-		<SearchPanel />
-		<TodoList todos={todoData}/>
+	return (<div className="todo-app">
+		<AppHeader toDo={1} done={3} />
+		<div className="top-panel d-flex">
+			<SearchPanel />
+			<ItemStatusFilter />
+		</div>
+		<TodoList todos={todoData} />
 	</div>);
 };
 
